d317881e9a33a868acc98716bbdbeb35417a5d49
liusong <liusong@obsidian-interactive.com>
2016-08-08 16:00:07 +0800

    增加多sheet表上传功能

--- Manage/upload.php
+++ Manage/upload.php
@@ -45,71 +45,75 @@ if(empty($_FILES['file_path']['name'])){
     $inputFileName = "$save_path";
     echo '上传文件中... ',pathinfo($inputFileName,PATHINFO_BASENAME),'<br />';
     $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-    $objWorksheet = $objPHPExcel->getActiveSheet();//取得总表数
-    $highestRow = $objWorksheet->getHighestRow();//取得总行数
-    echo 'highestRow='.$highestRow;
-    echo "<br>";
-    $highestColumn = $objWorksheet->getHighestColumn();
-    $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);//总列数
-    echo 'highestColumn='.$highestColumn;
-    echo "<br />";
-    echo 'highestColumnIndex='.$highestColumnIndex;
-    echo "<br />";
-    for ($row = 2;$row <= $highestRow;$row++)
-    {
-        $strs=array();
-        //注意highestColumnIndex的列数索引从0开始
-        for ($col = 0;$col < $highestColumnIndex;$col++)
+    $sheetCount = $objPHPExcel->getSheetCount();
+    echo 'sheetcouont='.$sheetCount;
+    for ($sheet = 0;$sheet < $sheetCount;$sheet++){
+        $objWorksheet = $objPHPExcel->getSheet($sheet);
+        $highestRow = $objWorksheet->getHighestRow();//取得总行数
+        echo 'highestRow='.$highestRow;
+        echo "<br>";
+        $highestColumn = $objWorksheet->getHighestColumn();
+        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);//总列数
+        echo 'highestColumn='.$highestColumn;
+        echo "<br />";
+        echo 'highestColumnIndex='.$highestColumnIndex;
+        echo "<br />";
+        for ($row = 2;$row <= $highestRow;$row++)
         {
-            $strs[$col] =$objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
-        }
-        $info = array(
-            'rid'=>"$strs[0]",
-            'title'=>"$strs[1]",
-            'status'=>"$strs[2]",
-            'result'=>"$strs[3]",
-            'stime'=>"$strs[4]",
-            'etime'=>"$strs[5]",
-            'number'=>"$strs[6]",
-            'name'=>"$strs[7]",
-            'depart'=>"$strs[8]",
-            'historyname'=>"$strs[9]",
-            'record'=>"$strs[10]",
-            'nowname'=>"$strs[11]",
-            'ctime'=>"$strs[12]",
-            'type'=>"$strs[13]",
-            'astime'=>"$strs[14]",
-            'aetime'=>"$strs[15]",
-            'days'=>"$strs[16]",
-            'reason'=>"$strs[17]",
-            'pic'=>"$strs[18]",
-        );
-        //从excel文件生成数组
+            $strs=array();
+            //注意highestColumnIndex的列数索引从0开始
+            for ($col = 0;$col < $highestColumnIndex;$col++)
+            {
+                $strs[$col] =$objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
+            }
+            $info = array(
+                'rid'=>"$strs[0]",
+                'title'=>"$strs[1]",
+                'status'=>"$strs[2]",
+                'result'=>"$strs[3]",
+                'stime'=>"$strs[4]",
+                'etime'=>"$strs[5]",
+                'number'=>"$strs[6]",
+                'name'=>"$strs[7]",
+                'depart'=>"$strs[8]",
+                'historyname'=>"$strs[9]",
+                'record'=>"$strs[10]",
+                'nowname'=>"$strs[11]",
+                'ctime'=>"$strs[12]",
+                'type'=>"$strs[13]",
+                'astime'=>"$strs[14]",
+                'aetime'=>"$strs[15]",
+                'days'=>"$strs[16]",
+                'reason'=>"$strs[17]",
+                'pic'=>"$strs[18]",
+            );
+            //从excel文件生成数组
 
-        if($conn){
-            $rid = $info['rid'];
-            $title = $info['title'];
-            $status = $info['status'];
-            $result = $info['result'];
-            $stime = $info['stime'];
-            $etime = $info['etime'];
-            $number = $info['number'];
-            $name = $info['name'];
-            $depart = $info['depart'];
-            $historyname = $info['historyname'];
-            $record = $info['record'];
-            $nowname = $info['nowname'];
-            $ctime = $info['ctime'];
-            $type = $info['type'];
-            $astime = $info['astime'];
-            $aetime = $info['aetime'];
-            $days = $info['days'];
-            $reason = $info['reason'];
-            $pic = $info['pic'];
-            mysqli_query($conn,"INSERT INTO details(rid,title,status,result,stime,etime,num,name,depart,historyname,record,nowname,ctime,type,astime,aetime,days,reason,pic) SELECT '$rid','$title','$status','$result','$stime','$etime','$number','$name','$depart','$historyname','$record','$nowname','$ctime','$type','$astime','$aetime','$days','$reason','$pic' FROM DUAL WHERE '$rid' NOT IN (SELECT rid FROM details)");
-            if(mysqli_errno($conn)){
-                echo mysqli_error($conn);
-            }//数据库插入以rid为标准不重复插入
+            if($conn){
+                $rid = $info['rid'];
+                $title = $info['title'];
+                $status = $info['status'];
+                $result = $info['result'];
+                $stime = $info['stime'];
+                $etime = $info['etime'];
+                $number = $info['number'];
+                $name = $info['name'];
+                $depart = $info['depart'];
+                $historyname = $info['historyname'];
+                $record = $info['record'];
+                $nowname = $info['nowname'];
+                $ctime = $info['ctime'];
+                $type = $info['type'];
+                $astime = $info['astime'];
+                $aetime = $info['aetime'];
+                $days = $info['days'];
+                $reason = $info['reason'];
+                $pic = $info['pic'];
+                mysqli_query($conn,"INSERT INTO details(rid,title,status,result,stime,etime,num,name,depart,historyname,record,nowname,ctime,type,astime,aetime,days,reason,pic) SELECT '$rid','$title','$status','$result','$stime','$etime','$number','$name','$depart','$historyname','$record','$nowname','$ctime','$type','$astime','$aetime','$days','$reason','$pic' FROM DUAL WHERE '$rid' NOT IN (SELECT rid FROM details)");
+                if(mysqli_errno($conn)){
+                    echo mysqli_error($conn);
+                }//数据库插入以rid为标准不重复插入
+            }
         }
     }
     echo "<script>alert('数据库导入成功！');parent.location.href='history.php';</script>";
